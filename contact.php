1585834639811b5df2973c69f61702a6087e019d
Matteo Currò <curromatteo@gmail.com>
2014-02-24 13:20:03 +0100

    Add link to blog

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@
 				<li id="mail"><a href="m&#97;i&#108;&#116;&#111;&#58;&#104;&#105;&#64;&#99;&#117;&#114;&#114;&#111;mat&#116;e&#111;.co&#109;?subject=Ciao Matteo!">Scrivermi una mail</a></li>
 				<li id="skype"><a href="skype:curromatteo?add">Contattarmi su Skype</a></li>
 				<li id="telefono" id="mail_telefono"><a href="http://eepurl.com/IsPnH" target="_blank">Chiamarmi</a></li>
-				<li class="delete">Leggere il blog</li>
+				<li><a href="http://www.kludge.it" title="Kludge - Web design in pillole">Leggere il blog</a></li>
 				<li id="mlist"><a href="http://eepurl.com/vfglb" target="_blank">Iscrivervi alla mailing list</a></li>
 			</ul>
 			<p>Grazie per il tempo e l’interesse, Matteo Curr&ograve;</p>
